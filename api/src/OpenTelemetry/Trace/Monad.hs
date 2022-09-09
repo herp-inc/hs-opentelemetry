1d45dae714484361aebb8cdf0ecf48d440e5916f
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2022-09-09 18:13:54 +0900

    add instances for monad-tracer

--- api/src/OpenTelemetry/Trace/Monad.hs
+++ api/src/OpenTelemetry/Trace/Monad.hs
@@ -42,6 +42,19 @@ import OpenTelemetry.Trace.Core
   )
 import Control.Monad.IO.Unlift
 import GHC.Stack
+import Control.Monad.Trans (MonadTrans(lift))
+import Control.Monad.Trans.Accum (AccumT)
+import Control.Monad.Cont (ContT)
+import Control.Monad.Identity (IdentityT)
+import qualified Control.Monad.Trans.RWS.CPS as C
+import qualified Control.Monad.RWS.Lazy as L
+import qualified Control.Monad.RWS.Strict as S
+import Control.Monad.Reader (ReaderT)
+import Control.Monad.Trans.Select (SelectT)
+import qualified Control.Monad.State.Lazy as L
+import qualified Control.Monad.State.Strict as S
+import qualified Control.Monad.Writer.Lazy as L
+import qualified Control.Monad.Writer.Strict as S
 
 -- | This is generally scoped by Monad stack to do different things
 class Monad m => MonadTracer m where
@@ -73,3 +86,39 @@ inSpan''
 inSpan'' cs n args f = do
   t <- getTracer
   OpenTelemetry.Trace.Core.inSpan'' t cs n args f
+
+instance (MonadTracer m, Monoid w) => MonadTracer (AccumT w m) where
+  getTracer = lift getTracer
+
+instance MonadTracer m => MonadTracer (ContT r m) where
+  getTracer = lift getTracer
+
+instance MonadTracer m => MonadTracer (IdentityT m) where
+  getTracer = lift getTracer
+
+instance MonadTracer m => MonadTracer (C.RWST r w s m) where
+  getTracer = lift getTracer
+
+instance (MonadTracer m, Monoid w) => MonadTracer (L.RWST r w s m) where
+  getTracer = lift getTracer
+
+instance (MonadTracer m, Monoid w) => MonadTracer (S.RWST r w s m) where
+  getTracer = lift getTracer
+
+instance MonadTracer m => MonadTracer (ReaderT r m) where
+  getTracer = lift getTracer
+
+instance MonadTracer m => MonadTracer (SelectT r m) where
+  getTracer = lift getTracer
+
+instance MonadTracer m => MonadTracer (L.StateT s m) where
+  getTracer = lift getTracer
+
+instance MonadTracer m => MonadTracer (S.StateT s m) where
+  getTracer = lift getTracer
+
+instance (MonadTracer m, Monoid w) => MonadTracer (L.WriterT w m) where
+  getTracer = lift getTracer
+
+instance (MonadTracer m, Monoid w) => MonadTracer (S.WriterT w m) where
+  getTracer = lift getTracer
