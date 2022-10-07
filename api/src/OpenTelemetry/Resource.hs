374f176f4c4d70eb4186cb19b64eb17ac458e8bb
Kazuki Okamoto <kazuki.okamoto@herp.co.jp>
2022-10-07 14:38:14 +0900

    Show log

--- api/src/OpenTelemetry/Resource.hs
+++ api/src/OpenTelemetry/Resource.hs
@@ -175,8 +175,7 @@ instance (KnownSymbol s) => MaterializeResource ('Just s) where
 data MaterializedResources = MaterializedResources
   { materializedResourcesSchema :: Maybe String
   , materializedResourcesAttributes :: Attributes
-  }
-  deriving (Show)
+  } deriving Show
 
 
 {- | A placeholder for 'MaterializedResources' when no resource information is
