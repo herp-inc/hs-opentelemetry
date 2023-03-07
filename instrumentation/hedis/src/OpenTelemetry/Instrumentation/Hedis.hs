fadf7b6d86df9d38506c5e0c62365c5d2171ed54
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2023-03-07 14:07:40 +0900

    hedis instrumentation

---
+++ instrumentation/hedis/src/OpenTelemetry/Instrumentation/Hedis.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module OpenTelemetry.Instrumentation.Hedis
+  ( Redis (Redis),
+    runRedis,
+    runRedis',
+  )
+where
+
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.IO.Unlift (MonadUnliftIO)
+import Control.Monad.Reader (MonadReader (ask), ReaderT (ReaderT, runReaderT))
+import qualified Database.Redis as Redis
+import qualified OpenTelemetry.Trace.Core as Otel (Tracer, inSpan,defaultSpanArguments)
+import qualified OpenTelemetry.Trace.Monad as Otel (getTracer, MonadTracer)
+
+newtype Redis a
+  = Redis (ReaderT Otel.Tracer Redis.Redis a)
+  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadUnliftIO, MonadFail, MonadReader Otel.Tracer)
+
+instance Redis.MonadRedis Redis where
+  liftRedis r = do
+    tracer <- ask
+    Otel.inSpan tracer "liftRedis" Otel.defaultSpanArguments $ lift r
+
+instance Redis.RedisCtx Redis (Either Redis.Reply) where
+  returnDecode = lift . Redis.returnDecode
+
+lift :: Redis.Redis a -> Redis a
+lift r = Redis $ ReaderT $ const r
+
+runRedis :: MonadIO m => Redis.Connection -> Otel.Tracer -> Redis a -> m a
+runRedis conn tracer (Redis m) = liftIO $ Redis.runRedis conn $ runReaderT m tracer
+
+runRedis' :: (Otel.MonadTracer m, MonadIO m) => Redis.Connection -> Redis a -> m a
+runRedis' conn redis = do
+  tracer <- Otel.getTracer
+  runRedis conn tracer redis
