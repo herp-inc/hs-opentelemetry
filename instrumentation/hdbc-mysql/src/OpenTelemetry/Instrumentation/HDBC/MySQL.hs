ebd7ab0fe8638bd7c74ba2109ff45217902886ca
Kazuki Okamoto <kazuki.okamoto@herp.co.jp>
2023-12-18 03:10:22 +0900

    Otel for HDBC and HDBC MySQL

---
+++ instrumentation/hdbc-mysql/src/OpenTelemetry/Instrumentation/HDBC/MySQL.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module OpenTelemetry.Instrumentation.HDBC.MySQL (
+  Orig.MySQLConnectInfo (..),
+  Otel.Connection,
+  connectMySQL,
+  Orig.defaultMySQLConnectInfo,
+  Orig.withRTSSignalsBlocked,
+) where
+
+import qualified Data.Text as Text
+import qualified Database.HDBC.MySQL as Orig
+import qualified OpenTelemetry.Attribute as Attr
+import qualified OpenTelemetry.Attribute.Attributes as Attr
+import qualified OpenTelemetry.Attribute.Key as Attr
+import qualified OpenTelemetry.Instrumentation.HDBC as Otel
+import qualified OpenTelemetry.Trace.Core as Otel
+
+
+connectMySQL :: Otel.TracerProvider -> Attr.Attributes -> Orig.MySQLConnectInfo -> IO Otel.Connection
+connectMySQL
+  tracerProvider
+  extraAttributes
+  connectInfo@Orig.MySQLConnectInfo
+    { Orig.mysqlHost
+    , Orig.mysqlUser
+    , Orig.mysqlDatabase
+    , Orig.mysqlPort
+    , Orig.mysqlUnixSocket
+    } = do
+    connection <- Orig.connectMySQL connectInfo
+    let port :: Maybe Word
+        transport :: Text.Text
+        (port, transport) =
+          if null mysqlUnixSocket
+            then (Just $ fromIntegral mysqlPort, "tcp")
+            else (Nothing, "unix")
+        attributes =
+          Otel.Attributes
+            { Otel.db_connectionString = Nothing
+            , Otel.db_system = "mysql"
+            , Otel.db_user = Just $ Text.pack mysqlUser
+            , Otel.network_peer_address = Just $ Text.pack mysqlHost
+            , Otel.network_peer_port = port
+            , Otel.network_transport = Just transport
+            , Otel.network_type = Nothing
+            , Otel.server_address = Just $ Text.pack mysqlHost
+            , Otel.server_port = port
+            }
+        extraAttributes' = Attr.insert Attr.db_name (Text.pack mysqlDatabase) extraAttributes
+    pure $ Otel.makeConnection connection tracerProvider attributes extraAttributes'
---
+++ instrumentation/hdbc/src/OpenTelemetry/Instrumentation/HDBC.hs
@@ -0,0 +1,193 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+
+module OpenTelemetry.Instrumentation.HDBC (
+  Connection (..),
+  Attributes (..),
+  makeConnection,
+) where
+
+import Data.Default.Class (def)
+import Data.Function ((&))
+import Data.Kind (Type)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Database.HDBC (
+  ConnWrapper (ConnWrapper),
+  IConnection (
+    clone,
+    commit,
+    dbServerVer,
+    dbTransactionSupport,
+    describeTable,
+    disconnect,
+    getTables,
+    hdbcClientVer,
+    hdbcDriverName,
+    prepare,
+    proxiedClientName,
+    proxiedClientVer,
+    rollback,
+    run
+  ),
+ )
+import GHC.Generics (Generic)
+import GHC.Stack (withFrozenCallStack)
+import qualified OpenTelemetry.Attribute as Attr
+import qualified OpenTelemetry.Attribute.Attributes as Attr
+import qualified OpenTelemetry.Attribute.Key as Attr
+import qualified OpenTelemetry.Trace.Core as Otel
+
+
+type Connection :: Type
+data Connection = Connection
+  { original :: ConnWrapper
+  , tracer :: Otel.Tracer
+  , attributes :: Attr.Attributes
+  }
+
+
+-- | Specification: https://opentelemetry.io/docs/specs/semconv/database/database-spans/
+type Attributes :: Type
+data Attributes = Attributes
+  { db_connectionString :: Maybe Text
+  , db_system :: Text
+  , db_user :: Maybe Text
+  , network_peer_address :: Maybe Text
+  , network_peer_port :: Maybe Word
+  , network_transport :: Maybe Text
+  , network_type :: Maybe Text
+  , server_address :: Maybe Text
+  , server_port :: Maybe Word
+  }
+  deriving stock (Show, Read, Eq, Ord, Generic)
+
+
+makeConnection ::
+  IConnection connection =>
+  -- | Original connection
+  connection ->
+  Otel.TracerProvider ->
+  Attributes ->
+  -- | Extra attributes
+  Attr.Attributes ->
+  Connection
+makeConnection original provider attributes extraAttributes =
+  Connection
+    { original = ConnWrapper original
+    , tracer = Otel.makeTracer provider "hs-opentelemetry-instrumentation-hdbc" Otel.tracerOptions
+    , attributes = Attr.union (convertAttributes attributes) extraAttributes
+    }
+
+
+convertAttributes :: Attributes -> Attr.Attributes
+convertAttributes
+  Attributes
+    { db_connectionString
+    , db_system
+    , db_user
+    , network_peer_address
+    , network_peer_port
+    , network_transport
+    , network_type
+    , server_address
+    , server_port
+    } =
+    Attr.empty
+      & insert Attr.db_connectionString db_connectionString
+      & insert Attr.db_system (Just db_system)
+      & insert Attr.db_user db_user
+      & insert Attr.network_peer_address network_peer_address
+      & insert Attr.network_peer_port (fromIntegral <$> network_peer_port)
+      & insert Attr.network_transport network_transport
+      & insert Attr.network_type network_type
+      & insert Attr.server_address server_address
+      & insert Attr.server_port (fromIntegral <$> server_port)
+    where
+      insert ::
+        Attr.IsAttribute a =>
+        Attr.Key a ->
+        Maybe a ->
+        Attr.Attributes ->
+        Attr.Attributes
+      insert _ Nothing = id
+      insert key (Just value) = Attr.insert key value
+
+
+instance IConnection Connection where
+  disconnect Connection {original, tracer, attributes} =
+    withFrozenCallStack $
+      Otel.inSpan tracer "disconnect" (defaultSpanArguments attributes) $
+        disconnect original
+
+
+  commit Connection {original, tracer, attributes} =
+    withFrozenCallStack $
+      Otel.inSpan tracer "commit" (defaultSpanArguments attributes) $
+        commit original
+
+
+  rollback Connection {original, tracer, attributes} =
+    withFrozenCallStack $
+      Otel.inSpan tracer "rollback" (defaultSpanArguments attributes) $
+        rollback original
+
+
+  run Connection {original, tracer, attributes} query params =
+    withFrozenCallStack $ do
+      let attributes' = Attr.insert Attr.db_statement (Text.pack query) attributes
+      Otel.inSpan tracer "run" (defaultSpanArguments attributes') $
+        run original query params
+
+
+  prepare Connection {original, tracer, attributes} query =
+    withFrozenCallStack $ do
+      let attributes' = Attr.insert Attr.db_statement (Text.pack query) attributes
+      Otel.inSpan tracer "prepare" (defaultSpanArguments attributes') $
+        prepare original query
+
+
+  clone Connection {original, tracer, attributes} =
+    withFrozenCallStack $
+      Otel.inSpan tracer "clone" (defaultSpanArguments attributes) $ do
+        conn <- clone original
+        pure $ Connection {original = conn, tracer, attributes}
+
+
+  hdbcDriverName Connection {original} = withFrozenCallStack $ hdbcDriverName original
+
+
+  hdbcClientVer Connection {original} = withFrozenCallStack $ hdbcClientVer original
+
+
+  proxiedClientName Connection {original} = withFrozenCallStack $ proxiedClientName original
+
+
+  proxiedClientVer Connection {original} = withFrozenCallStack $ proxiedClientVer original
+
+
+  dbServerVer Connection {original} = withFrozenCallStack $ dbServerVer original
+
+
+  dbTransactionSupport Connection {original} = withFrozenCallStack $ dbTransactionSupport original
+
+
+  getTables Connection {original, tracer, attributes} =
+    withFrozenCallStack $ do
+      Otel.inSpan tracer "getTables" (defaultSpanArguments attributes) $
+        getTables original
+
+
+  describeTable Connection {original, tracer, attributes} tableName =
+    withFrozenCallStack $ do
+      let attributes' = Attr.insert Attr.db_sql_table (Text.pack tableName) attributes
+      Otel.inSpan tracer "describeTable" (defaultSpanArguments attributes') $
+        describeTable original tableName
+
+
+defaultSpanArguments :: Attr.Attributes -> Otel.SpanArguments
+defaultSpanArguments attributes =
+  def {Otel.kind = Otel.Client, Otel.attributes = attributes}
